64f9579884788d2476050e3226b6997727330ecd
Pourya-Moghadam74 <p.moghadam74@gmail.com>
2025-12-25 22:25:32 -0500

    router fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   }, []);
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/chatbot">
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
