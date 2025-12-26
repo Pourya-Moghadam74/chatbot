bd02d8dcf66f2f0174a6c129e351726a5c1ec26e
Pourya-Moghadam74 <p.moghadam74@gmail.com>
2025-12-25 20:04:54 -0500

    db cred fixed

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -3,16 +3,22 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 
-load_dotenv()
+ENV = os.getenv("ENV", "local")
 
-DATABASE_URL = (
-    f"postgresql://"
-    f"{os.getenv('POSTGRES_USER')}:"
-    f"{os.getenv('POSTGRES_PASSWORD')}@"
-    f"{os.getenv('POSTGRES_HOST')}:"
-    f"{os.getenv('POSTGRES_PORT')}/"
-    f"{os.getenv('POSTGRES_DB')}"
-)
+if ENV == "local":
+    DATABASE_URL = (
+        f"postgresql://{os.getenv('POSTGRES_USER')}:"
+        f"{os.getenv('POSTGRES_PASSWORD')}@"
+        f"{os.getenv('POSTGRES_HOST')}:"
+        f"{os.getenv('POSTGRES_PORT')}/"
+        f"{os.getenv('POSTGRES_DB')}"
+    )
+else:
+    DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError("Database configuration missing")
+
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
@@ -29,3 +35,5 @@ def get_db():
         yield db
     finally:
         db.close()
+
+postgresql://chatbot_user:ny7PTBYqG0gXYr8qiLhud3bubnYSSGyV@dpg-d56traer433s73echq0g-a/chatbot_yu0i
